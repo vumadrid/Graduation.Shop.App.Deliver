5167f71055beccc7eb73c7c2e4c052f95f31aa20
LTVU <vumadridistazz@gmail.com>
2025-01-07 21:28:48 +0700

    [IMP] enviroments

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,7 +1,8 @@
-const SERVER_URL = 'http:// 172.20.10.5:8069/api';
-// const SERVER_URL = 'https://worthy-extremely-bonefish.ngrok-free.app/api';
-const GOOGLE_API_KEY = 'AIzaSyCzNP5qQql2a5y8lOoO-1yj1lj_tzjVImA';
+// const SERVER_URL = 'http://192.168.20.170:8069/api';
+const SERVER_URL = 'https://odoo-189962-0.cloudclusters.net/api';
+const GOOGLE_API_KEY = 'AIzaSyB9labulM1hoahFlmNih6WH0E8nw3dpWjQ';
 // const GOOGLE_API_KEY = 'AIzaSyB9labulM1hoahFlmNih6WH0E8nw3dpWjQ';
 
 
 export { SERVER_URL, GOOGLE_API_KEY };
+
